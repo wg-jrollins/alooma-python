8e959732c29ab45149cc4c5e30fe9b15972d96bc
Yonatan Kiron <yonatankiron@gmail.com>
2015-12-17 15:41:52 +0200

    fixes

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt", session=False)
+
+reqs = [str(ir.req) for ir in install_reqs]
+
 from distutils.core import setup
 
 setup(name='alooma',
-      version='0.1.3',
+      version='0.1.8',
       description='Alooma python API',
       author='Yonatan Kiron',
-      author_email='yonatan@alooma.io',
+      author_email='yonatankiron@gmail.com',
       packages=['alooma'],
-      install_requires=[
-        "urllib3>=1.13",
-        "requests>=2.9.0",
-        "paramiko>=1.16.0"
-      ],
+      install_requires=reqs,
       keywords=['alooma']
 )
