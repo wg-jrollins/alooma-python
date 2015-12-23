e01164890f74d34dee957e93f0c29f21b36945df
Yonatan Kiron <yonatankiron@gmail.com>
2015-12-23 16:30:52 +0200

    get metrics from one method

--- alooma/alooma.py
+++ alooma/alooma.py
@@ -19,6 +19,26 @@ DEFAULT_SETTINGS_EMAIL_NOTIFICATIONS = {
 DEFAULT_ENCODING = 'utf-8'
 
 RESTREAM_QUEUE_TYPE_NAME = "RESTREAM"
+METRICS_LIST = [
+    'EVENT_SIZE_AVG',
+    'EVENT_SIZE_TOTAL',
+    'EVENT_PROCESSING_RATE',
+    'CPU_USAGE',
+    'MEMORY_CONSUMED',
+    'MEMORY_LEFT',
+    'INCOMING_EVENTS',
+    'RESTREAMED_EVENTS',
+    'UNMAPPED_EVENTS',
+    'IGNORED_EVENTS',
+    'ERROR_EVENTS',
+    'LOADED_EVENTS_RATE',
+    'LATENCY_AVG',
+    'LATENCY_PERCENTILE_50',
+    'LATENCY_PERCENTILE_95',
+    'LATENCY_MAX',
+    'EVENTS_IN_PIPELINE',
+    'EVENTS_IN_TRANSIT'
+]
 
 
 class FailedToCreateInputException(Exception):
@@ -325,13 +345,28 @@ class Alooma(object):
         res = self.__send_request(requests.post, url, json=data)
         return res
 
-    def get_incoming_queue_metric(self, minutes):
-        url = self.rest_url + 'metrics?metrics=EVENTS_IN_PIPELINE' \
-                              '&from=-%dmin&resolution=%dmin' % (
-                                  minutes, minutes)
+    def get_metrics_by_names(self, metric_names, minutes):
+        if type(metric_names) != list and type(metric_names) == str:
+            metric_names = [metric_names]
+        elif type(metric_names) != str and type(metric_names) != list:
+            raise Exception("metric_names can be only from type `str` or "
+                            "`list`")
+        for metric_name in metric_names:
+            if metric_name not in METRICS_LIST:
+                raise Exception("Metrics '{name}' not exists, please use one or"
+                                " more of those: {metrics}".format(
+                                 name=metric_names, metrics=METRICS_LIST))
+
+        metrics_string = ",".join(metric_names)
+        url = self.rest_url + 'metrics?metrics=%s&from=-%dmin&resolution=%dmin'\
+                              '' % (metrics_string, minutes, minutes)
         res = self.__send_request(requests.get, url)
 
         response = parse_response_to_json(res)
+        return response
+
+    def get_incoming_queue_metric(self, minutes):
+        response = self.get_metrics_by_names("EVENTS_IN_PIPELINE", minutes)
         incoming = non_empty_datapoint_values(response)
         if incoming:
             return max(incoming)
@@ -344,22 +379,15 @@ class Alooma(object):
         the last X minutes
         :param minutes - number of minutes to check
         """
-        url = self.rest_url + 'metrics?metrics=UNMAPPED_EVENTS,IGNORED_EVENTS,'\
-                              'ERROR_EVENTS,LOADED_EVENTS_RATE' \
-                              '&from=-%dmin&resolution=%dmin' % (
-                                  minutes, minutes)
-
-        res = self.__send_request(requests.get, url)
-
-        response = parse_response_to_json(res)
+        response = self.get_metrics_by_names(['UNMAPPED_EVENTS',
+                                              'IGNORED_EVENTS',
+                                              'ERROR_EVENTS',
+                                              'LOADED_EVENTS_RATE'],
+                                             minutes)
         return tuple([sum(non_empty_datapoint_values([r])) for r in response])
 
     def get_restream_queue_metrics(self, minutes):
-        url = self.rest_url + 'metrics?metrics=EVENTS_IN_TRANSIT&from=-' \
-                              '%dmin&resolution=%dmin' % (minutes, minutes)
-        res = self.__send_request(requests.get, url)
-
-        response = parse_response_to_json(res)
+        response = self.get_metrics_by_names("EVENTS_IN_TRANSIT", minutes)
         return non_empty_datapoint_values(response)[0]
 
     def get_throughput_by_name(self, name):
@@ -368,21 +396,11 @@ class Alooma(object):
                 if x['name'] == name and not x['deleted']]
 
     def get_incoming_events_count(self, minutes):
-        url = self.rest_url + 'metrics?metrics=INCOMING_EVENTS&from=-' \
-                              '%dmin&resolution=%dmin' % (minutes, minutes)
-
-        res = self.__send_request(requests.get, url)
-
-        response = parse_response_to_json(res)
+        response = self.get_metrics_by_names("INCOMING_EVENTS", minutes)
         return sum(non_empty_datapoint_values(response))
 
     def get_average_event_size(self, minutes):
-        url = self.rest_url + 'metrics?metrics=EVENT_SIZE_AVG&from=-' \
-                              '%dmin&resolution=%dmin' % (minutes, minutes)
-        res = self.__send_request(requests.get, url)
-
-        response = parse_response_to_json(res)
-
+        response = self.get_metrics_by_names("EVENT_SIZE_AVG", minutes)
         values = non_empty_datapoint_values(response)
         if not values:
             return 0
@@ -390,12 +408,8 @@ class Alooma(object):
         return sum(values)/len(values)
 
     def get_max_latency(self, minutes):
-        url = self.rest_url + 'metrics?metrics=LATENCY_MAX&from=' \
-                              '-%dmin&resolution=%dmin' % (minutes, minutes)
         try:
-            res = self.__send_request(requests.get, url)
-
-            response = parse_response_to_json(res)
+            response = self.get_metrics_by_names("LATENCY_MAX", minutes)
             latencies = non_empty_datapoint_values(response)
             if latencies:
                 return max(latencies) / 1000
