56d23404c743cb72a6d7e7c429b98e1a6c73ce10
Rami Amar <amar.rami@gmail.com>
2015-12-29 14:30:05 +0200

    documentation, get_samples_status_codes, bug fix

--- alooma.py
+++ alooma.py
@@ -303,15 +303,40 @@ class Alooma(object):
         res = requests.put(url, str(sleep_time), **self.requests_params)
         return res
 
+    def get_samples_status_codes(self):
+        """
+        :return:    a list of status codes each event in Alooma may be tagged
+                    with. As Alooma supports more processing capabilities,
+                    status codes may be added. These status codes are used for
+                    sampling events according to the events' type & status.
+        """
+        url = self.rest_url + 'status-types'
+        res = requests.get(url, **self.requests_params)
+        return json.loads(res.content)
+
     def get_samples_stats(self):
+        """
+        :return:    a dictionary where the keys are names of event types,
+                    and each value is another dictionary which maps from status
+                    code to the amount of samples for that event type & status
+        """
         url = self.rest_url + 'samples/stats'
         res = requests.get(url, **self.requests_params)
         return json.loads(res.content.decode())
 
     def get_samples(self, event_type=None, error_codes=None):
+        """
+        :param event_type:  optional string containing an event type name
+        :param error_codes: optional list of strings containing event status
+                            codes. status codes may be any string returned by
+                            `get_sample_status_codes()`
+        :return:    a list of 10 samples. if event_type is passed, only samples
+                    of that event type will be returned. if error_codes is given
+                    only samples of those status codes are returned.
+        """
         url = self.rest_url + 'samples'
         if event_type:
-            url += '?event_type=%s' % event_type
+            url += '?eventType=%s' % event_type
         if error_codes and isinstance(error_codes, list):
             url += ''.join([ '&status=%s' % ec for ec in error_codes])
         res = requests.get(url, **self.requests_params)
@@ -330,6 +355,18 @@ class Alooma(object):
         return res
 
     def test_transform(self, sample, temp_transform=None):
+        """
+        :param sample:  a json string or a dict, representing a sample event
+        :param temp_transform: optional string containing transform code. if
+                        not provided, the currently deployed transform will be
+                        used.
+        :return:        the results of a test run of the temp_transform on the
+                        given sample. This returns a dictionary with the
+                        following keys:
+                            'output' - strings printed by the transform function
+                            'result' - the resulting event
+                            'runtime' - millis it took the function to run
+        """
         url = self.rest_url + 'transform/functions/run'
         if temp_transform is None:
             temp_transform = self.get_transform()
@@ -345,6 +382,15 @@ class Alooma(object):
         return json.loads(res.content)
 
     def test_transform_all_samples(self, event_type=None, status_code=None):
+        """
+        test many samples on the current transform at once
+        :param event_type:  optional string containing event type name
+        :param status_code: optional status code string
+        :return:    a list of samples (filtered by the event type & status code
+                    if provided), for each sample, a 'result' key is added which
+                    includes the result of the current transform function after
+                    it was run with the sample.
+        """
         curr_transform = self.get_transform()
         samples_stats = self.get_samples_stats()
         results = []
