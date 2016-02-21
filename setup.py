3eda8162c0c588ab72b71b884cf1e1adc291d334
Yonatan Kiron <yonatankiron@gmail.com>
2016-02-21 13:07:05 +0200

    "Updated alooma-python version from 0.1.19 to 0.1.20"

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 from distutils.core import setup
 
 setup(name='alooma',
-      version='0.1.19',
+      version='0.1.20',
       description='Alooma python API',
       author='Yonatan Kiron',
       author_email='yonatan@alooma.io',
