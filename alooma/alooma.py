142b75b989cf6f7eb45e45f37b1370b41096e726
Yonatan Kiron <yonatankiron@gmail.com>
2017-05-11 10:17:50 +0300

    updated to new endpoint

--- alooma/alooma.py
+++ alooma/alooma.py
@@ -1143,8 +1143,19 @@ class Client(object):
         Returns the number of events currently held in the Restream Queue
         :return: an int representing the number of events in the queue
         """
-        restream_node = self._get_node_by("type", RESTREAM_QUEUE_TYPE_NAME)
-        return restream_node["stats"]["availbleForRestream"]
+        warnings.warn("Call to deprecated function or method "
+                      "get_restream_queue_size (use get_events_to_restream).",
+                      DeprecationWarning, stacklevel=2)
+        return self.get_events_to_restream()
+
+    def get_events_to_restream(self):
+        """
+        Returns the number of events currently held in the Restream Queue
+        :return: an int representing the number of events in the queue
+        """
+        url = self.rest_url + 'plumbing/restream/eventsToRestream'
+        event_count = self.__send_request(requests.get, url)
+        return event_count
 
     def get_scheduled_queries(self):
         """
